a08f15fd2282c2dd63ece45e70983eaf7fb28450
Jpec57 <jean.paul.bella@hotmail.fr>
2020-05-09 23:15:53 +0200

    Trying to add lessons

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -3,7 +3,15 @@ import './EditorPage.scss';
 import MdEditor from 'react-markdown-editor-lite'
 import MarkdownIt from 'markdown-it';
 import 'react-markdown-editor-lite/lib/index.css';
+import {makePostRequest} from "../../utils/rest";
 
+type Lesson = {
+    title: String,
+    description: String | null,
+    banniere: String | null,
+    videoUrl: String | null,
+    content: String
+};
 
 const EditorPage = () => {
     const text = `
@@ -42,27 +50,85 @@ const EditorPage = () => {
 
     const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-    const [code, setCode] = React.useState(
-        text
-    );
-    function handleEditorChange({html, text}: {html: any, text:any}) {
-        console.log('handleEditorChange', html, text);
+    const [isSent, setIsSent] = useState(false);
+    const [code, setCode] = React.useState(text);
+    const [title, setTitle] = React.useState('This is a title');
+    const [description, setDescription] = React.useState('This is a small description on a lesson that does not exist yet. But believe me, when it will, it will be awesome. You have my word.');
+    const [banniereUrl, setBanniereUrl] = React.useState('');
+    const [videoUrl, setVideoUrl] = React.useState('https://www.youtube.com/watch?v=An6sKwhUH48');
+
+
+    const handleEditorChange = ({html, text}: { html: any, text: any }) => {
         setCode(text);
-    }
+    };
+
+    const checkForm = (lesson: Lesson): boolean => {
+        const {title, content} = lesson;
+        if (title.length < 3) {
+            return false;
+        }
+        return content.length >= 3;
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const lesson: Lesson = {
+            title: title,
+            description: description,
+            content: code,
+            banniere: banniereUrl,
+            videoUrl: videoUrl
+        };
+
+        const isFormValid = checkForm(lesson);
+        if (isFormValid) {
+            makePostRequest("/lessons", lesson).then((res: any) => {
+                console.log(res);
+                setIsSent(true);
+            });
+        }
+    };
+
+
+    const renderForm = () => {
+        return (<form onSubmit={handleSubmit}>
+
+                <div className="flex-vertical">
+                    <div className="flex-vertical mb-20">
+
+                    <span>Title</span>
+                    <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+
+                    <span>Description</span>
+                    <input value={description} onChange={(e) => setDescription(e.target.value)}/>
+
+                    <span>Cover image (optionnal)</span>
+                    <input value={banniereUrl} onChange={(e) => setBanniereUrl(e.target.value)}/>
+
+                    <span>Header video (optionnal)</span>
+                    <input value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)}/>
+                    </div>
+
+                    <div className="flex-horizontal">
+                        <MdEditor
+                            markdownClass="md-editor"
+                            value={code}
+                            renderHTML={(text) => mdParser.render(text)}
+                            onChange={handleEditorChange}
+                        />
+                    </div>
+                </div>
+                <button id="editor-send-button">
+                    SEND
+                </button>
+            </form>
+        );
+    };
 
 
     return (
         <div className="container">
-            <div className="flex-horizontal">
-
-                <MdEditor
-                    markdownClass="md-editor"
-                    value={code}
-                    renderHTML={(text) => mdParser.render(text)}
-                    onChange={handleEditorChange}
-                />
-            </div>
-
+            {isSent ? <span>GG</span> : renderForm()}
         </div>
     );
 };
